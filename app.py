dfece4042040ecd063fede720f0d6565d1c4e3fc
Ahsen Khaliq <akhaliq@users.noreply.huggingface.co>
2022-09-21 21:21:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ def inference(audio):
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
     
     _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
     
-    options = whisper.DecodingOptions()
+    options = whisper.DecodingOptions(fp16 = False)
     result = whisper.decode(model, mel, options)
     
     print(result.text)
